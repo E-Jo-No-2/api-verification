ea2213ac4231106293e2e1f57e73bc01176f6781
kim Do Hyung <bravolll@naver.com>
2024-11-21 16:29:12 +0900

    김도형20241121

---
+++ src/main/java/DTO/SpotDTO.java
@@ -0,0 +1,51 @@
+package DTO;
+
+
+public class SpotDTO {
+
+    private Integer spotId;
+    private String spotName;
+    private Double longitude;
+    private Double latitude;
+
+    // 생성자
+    public SpotDTO(Integer spotId, String spotName, Double longitude, Double latitude) {
+        this.spotId = spotId;
+        this.spotName = spotName;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    // Getters and Setters
+    public Integer getSpotId() {
+        return spotId;
+    }
+
+    public void setSpotId(Integer spotId) {
+        this.spotId = spotId;
+    }
+
+    public String getSpotName() {
+        return spotName;
+    }
+
+    public void setSpotName(String spotName) {
+        this.spotName = spotName;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+}
---
+++ src/main/java/Entity/ThemeEntity.java
@@ -0,0 +1,35 @@
+package Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Theme")
+public class ThemeEntity {
+
+    @Id
+    @Column(name = "theme_name", nullable = false, length = 50)
+    private String themeName;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    // Getter and Setter
+    public String getThemeName() {
+        return themeName;
+    }
+
+    public void setThemeName(String themeName) {
+        this.themeName = themeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
