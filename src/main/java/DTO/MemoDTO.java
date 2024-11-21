fa239b3d9d047b5501ca0da509612290b2b986cf
서진혁 <daniel0522@naver,comgit config --global user.nameSeo>
2024-11-21 16:47:00 +0900

    commit

---
+++ src/main/java/DTO/MemoDTO.java
@@ -0,0 +1,37 @@
+package DTO;
+
+public class MemoDTO {
+    private int memo_id;
+
+    public int getMemo_id() {
+        return memo_id;
+    }
+
+    public void setMemo_id(int memo_id) {
+        this.memo_id = memo_id;
+    }
+
+    public int getPlanner_id() {
+        return planner_id;
+    }
+
+    public void setPlanner_id(int planner_id) {
+        this.planner_id = planner_id;
+    }
+
+    public String getMemo_content() {
+        return memo_content;
+    }
+
+    public void setMemo_content(String memo_content) {
+        this.memo_content = memo_content;
+    }
+
+    private int planner_id;
+    private String memo_content;
+
+
+
+
+
+}
---
+++ src/main/java/DTO/ReviewDTO.java
@@ -0,0 +1,62 @@
+package DTO;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+public class ReviewDTO {
+
+    private int review_id;
+    private String user_id;
+    private int spot_id;
+    private double rating;
+    private String comment;
+    private Timestamp create_time;
+
+    public int getReview_id() {
+        return review_id;
+    }
+
+    public void setReview_id(int review_id) {
+        this.review_id = review_id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getSpot_id() {
+        return spot_id;
+    }
+
+    public void setSpot_id(int spot_id) {
+        this.spot_id = spot_id;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Timestamp getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Timestamp create_time) {
+        this.create_time = create_time;
+    }
+}
