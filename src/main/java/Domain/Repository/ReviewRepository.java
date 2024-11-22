e191834adf5400e807f79f83e3997ecc9c60825f
서진혁 <daniel0522@naver,comgit config --global user.nameSeo>
2024-11-23 08:18:07 +0900

    commit

--- src/main/java/Domain/Repository/ReviewRepository.java
+++ src/main/java/Domain/Repository/ReviewRepository.java
@@ -1,6 +1,6 @@
 package Domain.Repository;
 
-import Domain.Entity.ReviewEntity;
+//import Domain.Entity.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,12 +9,3 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Integer> {
 
-    // user_id로 리뷰조회
-    List<ReviewEntity> findByUserId(String user_id);
-
-    // spot_id로 리뷰조회
-    List<ReviewEntity> findBySpotId(int spot_id);
-
-    // spot_id와 user_id를 통해 리뷰조회
-    List<ReviewEntity> findByUserIdAndSpotId(String user_id, int spot_id);
-}
