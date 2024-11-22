a5587f4a5d06c5f983b5101a986e2f1ee53f6bf4
kim Do Hyung <bravolll@naver.com>
2024-11-22 11:48:57 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/DTO/UserDTO.java
@@ -0,0 +1,62 @@
+package DTO;
+
+import java.util.Date;
+
+public class UserDTO {
+
+    private String userId;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private String phoneNumber;
+
+    // 생성자
+    public UserDTO(String userId, String name, String email, Date birthDate, String phoneNumber) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getters and Setters
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
