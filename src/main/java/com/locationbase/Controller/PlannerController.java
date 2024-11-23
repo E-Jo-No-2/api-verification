52864f7344be737dd2b3961a0a75b6bf591fa4ec
서진혁 <daniel0522@naver,comgit config --global user.nameSeo>
2024-11-23 14:04:00 +0900

    commit

--- src/main/java/com/locationbase/Controller/PlannerController.java
+++ src/main/java/com/locationbase/Controller/PlannerController.java
@@ -1,4 +1,14 @@
 package com.locationbase.Controller;
 
+import com.locationbase.DTO.PlannerDTO;
+import com.locationbase.Service.PlannerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/planners")
 public class PlannerController {
+
 }
--- src/main/java/com/locationbase/DTO/PlannerDTO.java
+++ src/main/java/com/locationbase/DTO/PlannerDTO.java
@@ -1,44 +1,12 @@
 package com.locationbase.DTO;
+import lombok.Data;
+
 import java.time.LocalDate;
 
-
+@Data
 public class PlannerDTO {
     private int planner_id;
     private String user_id;
     private String theme_name;
     private LocalDate start_date;
-
-
-    public int getPlanner_id() {
-        return planner_id;
-    }
-
-    public void setPlanner_id(int planner_id) {
-        this.planner_id = planner_id;
-    }
-
-    public String getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getTheme_name() {
-        return theme_name;
-    }
-
-    public void setTheme_name(String theme_name) {
-        this.theme_name = theme_name;
-    }
-
-    public LocalDate getStart_date() {
-        return start_date;
-    }
-
-    public void setStart_date(LocalDate start_date) {
-        this.start_date = start_date;
-    }
-
 }
