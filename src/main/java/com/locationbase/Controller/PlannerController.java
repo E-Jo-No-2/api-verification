8cc63dac25872c09d89084ccde7673a92e600334
서진혁 <daniel0522@naver,comgit config --global user.nameSeo>
2024-11-24 20:09:23 +0900

    commit

--- src/main/java/com/locationbase/Controller/PlannerController.java
+++ src/main/java/com/locationbase/Controller/PlannerController.java
@@ -10,24 +10,4 @@ import java.util.List;
 @RequestMapping("/planners")
 public class PlannerController {
 
-    @Autowired
-    private PlannerService plannerService;
-
-    // Create a new Planner with a theme and landmark
-    @PostMapping
-    public PlannerDTO createPlanner(@RequestBody PlannerDTO plannerDTO) {
-        return plannerService.createPlanner(plannerDTO);
-    }
-
-    // Update an existing Planner
-    @PutMapping("/{planner_id}")
-    public PlannerDTO updatePlanner(@PathVariable int planner_id, @RequestBody PlannerDTO plannerDTO) {
-        return plannerService.updatePlanner(planner_id, plannerDTO);
-    }
-
-    // Delete a Planner
-    @DeleteMapping("/{planner_id}")
-    public void deletePlanner(@PathVariable int planner_id) {
-        plannerService.deletePlanner(planner_id);
-    }
 }
