c18543997971863920f79ee3808ca3f992108f3a
서진혁 <daniel0522@naver,comgit config --global user.nameSeo>
2024-11-26 09:40:41 +0900

    commit

--- src/main/java/com/locationbase/Entity/ReviewEntity.java
+++ src/main/java/com/locationbase/Entity/ReviewEntity.java
@@ -1,9 +1,11 @@
 package com.locationbase.Entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
 @Entity
 @Table(name = "Review")
 public class ReviewEntity {
