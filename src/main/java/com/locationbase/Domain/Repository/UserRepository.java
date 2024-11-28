ac71c80c6d830af8971cd97c89d5027d99ef9a5c
서진혁 <daniel0522@naver,comgit config --global user.nameSeo>
2024-11-28 19:27:05 +0900

    commit

--- src/main/java/com/locationbase/Domain/Repository/UserRepository.java
+++ src/main/java/com/locationbase/Domain/Repository/UserRepository.java
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, String> {
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     // 이메일을 기준으로 사용자 조회
     Optional<UserEntity> findByEmail(String email);
 
     // 사용자의 아이디로 조회 (기본 제공 메소드)
-    Optional<UserEntity> findById(String userId);
+    Optional<UserEntity> findById(Integer user_id);
 }
