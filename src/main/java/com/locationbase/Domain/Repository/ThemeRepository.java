ee4aa907f10a475b3e42303f75b5fc9576e05fc3
서진혁 <daniel0522@naver,comgit config --global user.nameSeo>
2024-11-28 13:23:16 +0900

    commit

--- src/main/java/com/locationbase/Domain/Repository/ThemeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.locationbase.Domain.Repository;
-
-import com.locationbase.Entity.ThemeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ThemeRepository extends JpaRepository<ThemeEntity, String> {
-}
