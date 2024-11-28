5ffea4315c762c8089c4908a639a00f0c0473aa9
김하람 <uj02184@kw.ac.kr>
2024-11-28 20:39:26 +0900

    기본 설정

--- src/main/java/com/locationbase/Domain/Repository/SpotRepository.java
+++
@@ -1,8 +0,0 @@
-package com.locationbase.Domain.Repository;
-
-import com.locationbase.Entity.SpotEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpotRepository extends JpaRepository<SpotEntity, Integer> {}
--- src/main/java/com/locationbase/Domain/Repository/ThemeRepository.java
+++ src/main/java/com/locationbase/Domain/Repository/ThemeRepository.java
@@ -1,6 +1,5 @@
 package com.locationbase.Domain.Repository;
 
-import com.locationbase.Entity.ThemeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
