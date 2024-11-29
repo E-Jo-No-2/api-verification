2bec111071f19686498180db0aa132ff6b51318a
kim Do Hyung <bravolll@naver.com>
2024-11-29 10:40:15 +0900

    김도형/업데이트

--- src/main/java/com/locationbase/Application.java
+++ src/main/java/com/locationbase/Application.java
@@ -3,7 +3,8 @@ package com.locationbase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.locationbase.Controller", "com.locationbase.Service", "com.locationbase.Domain", "com.locationbase.DTO"})
+@SpringBootApplication(scanBasePackages = {"com.locationbase.Controller",
+        "com.locationbase.Service", "com.locationbase.Domain", "com.locationbase.DTO"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
