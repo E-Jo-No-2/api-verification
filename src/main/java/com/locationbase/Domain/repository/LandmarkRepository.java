fe0380ec0c5956a4b3912a5cce06290d05feff30
jseongyu <jseongyu@gmail.com>
2024-12-09 22:19:56 +0900

    landmark 페이지 살리기 3

---
+++ src/main/java/com/locationbase/Domain/repository/LandmarkRepository.java
@@ -0,0 +1,9 @@
+package com.locationbase.domain.repository;
+
+import com.locationbase.entity.LandmarkEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LandmarkRepository extends JpaRepository<LandmarkEntity, Integer> {
+}
