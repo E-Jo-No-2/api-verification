fd2dc34e4aa21fef0ff9b6ad6c63fe81b12c6ab4
김하람 <uj02184@kw.ac.kr>
2024-12-10 09:22:12 +0900

    commit

--- src/main/java/com/locationbase/Domain/repository/ReviewRepository.java
+++ src/main/java/com/locationbase/Domain/repository/ReviewRepository.java
@@ -2,10 +2,13 @@ package com.locationbase.domain.repository;
 
 import com.locationbase.entity.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Integer> {
 
-
+    @Query("SELECT AVG(r.rating) AS averageRating, COUNT(r) AS reviewCount " +
+            "FROM ReviewEntity r WHERE r.place.placeId = ?1")
+    Object[] getAverageRatingByPlaceId(int placeId);
 }
