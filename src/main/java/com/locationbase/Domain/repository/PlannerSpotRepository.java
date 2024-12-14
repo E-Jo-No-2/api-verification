12b7e8ce92f38c410d4a04e6eb9d5aaf302b90a5
김하람 <uj02184@kw.ac.kr>
2024-12-14 13:44:58 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/locationbase/service/DirectionsService.java
    #	src/main/resources/static/js/map.js

--- src/main/java/com/locationbase/Domain/repository/PlannerSpotRepository.java
+++ src/main/java/com/locationbase/Domain/repository/PlannerSpotRepository.java
@@ -3,8 +3,10 @@ package com.locationbase.domain.repository;
 import com.locationbase.entity.PlannerSpotEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +15,5 @@ public interface PlannerSpotRepository extends JpaRepository<PlannerSpotEntity, 
     // 플래너 ID에 따른 최대 방문 순서 조회
     @Query("SELECT MAX(ps.visitOrder) FROM PlannerSpotEntity ps WHERE ps.planner.plannerId = :plannerId")
     Optional<Integer> findMaxVisitOrderByPlannerId(int plannerId);
+    List<PlannerSpotEntity> findByPlanner_PlannerIdOrderByVisitOrderAsc(@Param("plannerId") Integer plannerId);
 }
